16c766d347192d59864cc62c40d6848a62fd2870
GabyY1999 <gaby67gallard@gmail.com>
2024-06-24 03:39:19 -0300

    Update sensors.js

--- ejercicio1/sensors.js
+++ ejercicio1/sensors.js
@@ -1,4 +1,20 @@
-class Sensor {}
+class Sensor {
+    constructor(id, name, type, value, unit, updated_at) {
+        //propiedades
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.value = value;
+        this.unit = unit;
+        this.updated_at = updated_at;
+    }
+
+    set updateValue(newValue) {
+        this.value = newValue;
+        this.updated_at = new Date().toISOString();
+    }
+
+}
 
 class SensorManager {
     constructor() {
@@ -33,7 +49,23 @@ class SensorManager {
         }
     }
 
-    async loadSensors(url) {}
+    async loadSensors(url) {
+        try {
+            const response = await fetch(url);
+            const sensorsData = await response.json();
+            this.sensors = sensorsData.map(sensorData => new Sensor(
+                sensorData.id,
+                sensorData.name,
+                sensorData.type,
+                sensorData.value,
+                sensorData.unit,
+                sensorData.updated_at
+            ));
+            this.render();
+        } catch (error) {
+            console.error('Error loading sensors:', error);
+        }
+    }
 
     render() {
         const container = document.getElementById("sensor-container");
